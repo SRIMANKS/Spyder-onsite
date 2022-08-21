de7bc40f530e6efa7c18150e04beb27a3b353023
K S SRISOWRIRAJAN <kssrisowrirajan@gmail.com>
2022-08-21 17:00:53 +0530

    updated

--- front-end/passwordgenerator/script.js
+++ front-end/passwordgenerator/script.js
@@ -2,4 +2,9 @@ const input = document.querySelector('input');
 
 const button = document.querySelector('button');
 
+button.addEventListener('click', function() {
+    input.value = Math.random().toString(36).substring(2,10);
+});
+
+
 
