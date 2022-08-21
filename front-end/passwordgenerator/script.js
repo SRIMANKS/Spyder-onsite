5c172df8f3b1127cb65fa72054fcc4d5f2bc5aca
K S SRISOWRIRAJAN <kssrisowrirajan@gmail.com>
2022-08-21 19:02:26 +0530

    finsihed password

--- front-end/passwordgenerator/script.js
+++ front-end/passwordgenerator/script.js
@@ -1,9 +1,21 @@
 const input = document.querySelector('input');
-
+const option = document.querySelector('#option');
 const button = document.querySelector('button');
-
+const length = document.querySelector('#length');
 button.addEventListener('click', function() {
-    input.value = Math.random().toString(36).substring(2,10);
+    console.log(option.value);
+    if(option.value==="lc"){
+        input.value = Math.random().toString(36).substring(2,length.value+2).toLowerCase();
+    }
+    else if(option.value==="uc"){
+        input.value = Math.random().toString(36).substring(2,length.value+2).toUpperCase();
+    }
+    else if(option.value==="n"){
+        input.value = Math.random().toString(9).substring(2,length.value+2);
+    }
+    else if(option.value==="s"){
+        input.value = Math.random().toString(36).substring(2,length.value+2);
+    }
 });
 
 
