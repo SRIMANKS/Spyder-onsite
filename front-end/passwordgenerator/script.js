76b2ede3aa72c2e374eb3e468fa9176aa3f2a355
K S SRISOWRIRAJAN <kssrisowrirajan@gmail.com>
2022-08-21 19:13:31 +0530

    Update script.js

--- front-end/passwordgenerator/script.js
+++ front-end/passwordgenerator/script.js
@@ -3,18 +3,18 @@ const option = document.querySelector('#option');
 const button = document.querySelector('button');
 const length = document.querySelector('#length');
 button.addEventListener('click', function() {
-    console.log(option.value);
+
     if(option.value==="lc"){
-        input.value = Math.random().toString(36).substring(2,length.value+2).toLowerCase();
+        input.value = Math.random().toString(36).substring(2,parseInt(length.value) + 2).toLowerCase();
     }
     else if(option.value==="uc"){
-        input.value = Math.random().toString(36).substring(2,length.value+2).toUpperCase();
+        input.value = Math.random().toString(36).substring(2,parseInt(length.value) + 2).toUpperCase();
     }
     else if(option.value==="n"){
-        input.value = Math.random().toString(9).substring(2,length.value+2);
+        input.value = Math.random().toString(9).substring(2,parseInt(length.value) + 2);
     }
     else if(option.value==="s"){
-        input.value = Math.random().toString(36).substring(2,length.value+2);
+        input.value = Math.random().toString(36).substring(2,parseInt(length.value) + 2);
     }
 });
 
